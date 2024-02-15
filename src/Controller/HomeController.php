0bd396ed8c05b64483ea68fd50f2baf3a7bddfd3
Sylang <sylaang@outlook.fr>
2024-02-15 16:44:12 +0100

    resposive

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Récupérer la largeur de l'écran à partir de la requête
+        $screenWidth = $request->query->get('screenWidth', 0);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'screen_width' => $screenWidth,
         ]);
     }
 }
